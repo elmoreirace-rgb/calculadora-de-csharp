4d6723486ac1b5958c3bd67b7f0a5619d2caf05a
edu_programador <elmoreira.ce@gmail.com>
2025-11-23 18:06:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Sistema
+{
+    static void Main(string[] args)
+    {
+        //falar o programa
+        Console.WriteLine("calculadora");
+        Console.WriteLine("----------------------------------------");
+        
+        while (true)
+        {
+            //pedir os numeros
+            Console.Write("qual é o primero numero: ");
+            int numero1 = int.Parse(Console.ReadLine());
+            Console.Write("qual é o segundo numero: ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            //falar as opições
+            Console.WriteLine("escolha a operação: ");
+            Console.WriteLine("1 para somar");
+            Console.WriteLine("2 para subtrair");
+            Console.WriteLine("3 para multiplicar");
+            Console.WriteLine("4 para dividir");
+        
+            //perguntar a operação
+            Console.Write("qual a operação: ");
+            int operacao = int.Parse(Console.ReadLine());
+
+            //calcular
+            if (operacao == 1)
+            {
+                Console.WriteLine(numero1 + numero2);
+            }
+            else if (operacao == 2)
+            {
+                Console.WriteLine(numero1 - numero2);
+            }
+            else if (operacao == 3)
+            {
+                Console.WriteLine(numero1 * numero2);
+            }
+            else if (operacao == 4)
+            {
+                Console.WriteLine(numero1 / numero2);
+            }
+            else
+            {
+                Console.WriteLine("erro");
+                break;
+            }
+            
+        }
+
+    }
+}
